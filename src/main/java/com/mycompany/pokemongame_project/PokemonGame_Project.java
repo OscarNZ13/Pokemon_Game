a5351c2c9d37692d77b558f6cd983a5930a70bcf
EstivnQR <126968413+EstivenQR@users.noreply.github.com>
2023-06-22 16:38:59 +0200

    nombre v5

--- src/main/java/com/mycompany/pokemongame_project/PokemonGame_Project.java
+++ src/main/java/com/mycompany/pokemongame_project/PokemonGame_Project.java
@@ -3,7 +3,11 @@ package com.mycompany.pokemongame_project;
 public class PokemonGame_Project {
 
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         Pokemon P1 = new Pokemon("Pikachu", 150, 75, 60, PokemonType.Fire, PokemonType.Fire, PokemonType.Water);
+=======
+        Pokemon P1 = new Pokemon("Bulbasor", 150, 75, 60, PokemonType.Fire, PokemonType.Fire, PokemonType.Water);
+>>>>>>> Stashed changes
         System.out.println(P1.getType());
     }
 }
