4e91cf742b770eb734292163b6c41b6975c2390d
EstivnQR <126968413+EstivenQR@users.noreply.github.com>
2023-06-22 16:10:58 +0200

    Cambio nombre

--- src/main/java/com/mycompany/pokemongame_project/PokemonGame_Project.java
+++ src/main/java/com/mycompany/pokemongame_project/PokemonGame_Project.java
@@ -3,7 +3,7 @@ package com.mycompany.pokemongame_project;
 public class PokemonGame_Project {
 
     public static void main(String[] args) {
-        Pokemon P1 = new Pokemon("Picachu", 150, 75, 60, PokemonType.Fire, PokemonType.Fire, PokemonType.Water);
+        Pokemon P1 = new Pokemon("Charmander", 150, 75, 60, PokemonType.Fire, PokemonType.Fire, PokemonType.Water);
 
         System.out.println(P1.getType());
     }
