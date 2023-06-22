4be55972af55201487d35b6c92285c58a6c1eab4
OscarNZ13 <133930637+OscarNZ13@users.noreply.github.com>
2023-06-22 17:09:19 -0600

    Formato al documento

--- src/main/java/com/mycompany/pokemongame_project/PokemonGame_Project.java
+++ src/main/java/com/mycompany/pokemongame_project/PokemonGame_Project.java
@@ -5,11 +5,11 @@ public class PokemonGame_Project {
     public static void main(String[] args) {
 
         Pokemon P1 = new Pokemon("Raichu", 150, 75, 60, PokemonType.Fire, PokemonType.Fire, PokemonType.Water);
-        Pokemon P3 = new Pokemon("Charizard", 150, 75, 60, PokemonType.Fire, PokemonType.Fire, PokemonType.Water);
-
 
         Pokemon P2 = new Pokemon("Pikachu", 170, 75, 60, PokemonType.Normal, PokemonType.Fire, PokemonType.Water);
 
+        Pokemon P3 = new Pokemon("Charizard", 150, 75, 60, PokemonType.Fire, PokemonType.Fire, PokemonType.Water);
+        
         System.out.println(P1.getType());
     }
 }
