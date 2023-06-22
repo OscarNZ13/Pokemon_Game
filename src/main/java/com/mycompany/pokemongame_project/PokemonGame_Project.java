6c151d0cc6f89751b54051786e3663c5d148aa82
EstivnQR <126968413+EstivenQR@users.noreply.github.com>
2023-06-22 17:06:35 +0200

    Update PokemonGame_Project.java

--- src/main/java/com/mycompany/pokemongame_project/PokemonGame_Project.java
+++ src/main/java/com/mycompany/pokemongame_project/PokemonGame_Project.java
@@ -5,6 +5,8 @@ public class PokemonGame_Project {
     public static void main(String[] args) {
 
         Pokemon P1 = new Pokemon("Raichu", 150, 75, 60, PokemonType.Fire, PokemonType.Fire, PokemonType.Water);
+        Pokemon P3 = new Pokemon("Charizard", 150, 75, 60, PokemonType.Fire, PokemonType.Fire, PokemonType.Water);
+
 
         System.out.println(P1.getType());
     }
