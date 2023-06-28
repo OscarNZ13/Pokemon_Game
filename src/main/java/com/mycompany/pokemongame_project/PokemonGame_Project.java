57dd6002177ad38e6dc80e7250ed066e8ee160e6
EstivnQR <126968413+EstivenQR@users.noreply.github.com>
2023-06-28 15:45:11 +0200

    Menu inicio Frame
    Si quieren que el Menu ejecute la musica tienen que ir a la parte de menu y donde dice ruta poner la ubicacion donde tienen el proyecto guardado, el formato de la ruta tiene que ser igual al que esta ahi

--- src/main/java/com/mycompany/pokemongame_project/PokemonGame_Project.java
+++ src/main/java/com/mycompany/pokemongame_project/PokemonGame_Project.java
@@ -3,6 +3,10 @@ package com.mycompany.pokemongame_project;
 public class PokemonGame_Project {
 
     public static void main(String[] args) {
+        
+        Menu menu = new Menu();
+        menu.setLocationRelativeTo(null);
+        menu.setVisible(true);
 
         Snorlax_N P1 = new Snorlax_N(AllPokemons.Snorlax, 160, 70, 60, 70, 50, PokemonType.Fire, PokemonType.Normal, PokemonType.Fire);
         Chandelure_F P2 = new Chandelure_F(AllPokemons.Chandelure, 150, 60, 50, 75, 60, PokemonType.Fire, PokemonType.Normal, PokemonType.Fire);
