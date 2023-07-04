91086685baebe7f6d5db33c70b610718ed946d97
rodrv <valeriarodriguez09@hotmail.com>
2023-07-03 19:52:17 -0600

    Prueba_métodoSpecialAttack

--- src/main/java/com/mycompany/pokemongame_project/Charizard_F.java
+++ src/main/java/com/mycompany/pokemongame_project/Charizard_F.java
@@ -1,6 +1,6 @@
 package com.mycompany.pokemongame_project;
 
-public class Charizard_F extends Pokemon implements IPokemon_Actions{
+public class Charizard_F extends Pokemon implements IPokemon_Actions {
 
     public Charizard_F(AllPokemons Name, int HP, int Attack, int Defense, int SpecialAttack, int SpecialDefense, PokemonType Type, PokemonType Strong, PokemonType Weak) {
         super(Name, HP, Attack, Defense, SpecialAttack, SpecialDefense, Type, Strong, Weak);
@@ -8,11 +8,11 @@ public class Charizard_F extends Pokemon implements IPokemon_Actions{
 
     @Override
     public void Attack(Pokemon p) {
-        int damage = getAttack()-p.getDefense(); //restar el atque a la defense del pokemon
-        int rest = p.getHP()-damage;
-        System.out.println("Tu oponente" + getName() + "ha atacado a tu Pokemon"+p.getName()+"con un daño de" +damage );
+        int damage = getAttack() - p.getDefense(); //restar el atque a la defense del pokemon
+        int rest = p.getHP() - damage;
+        System.out.println("Tu oponente" + getName() + "ha atacado a tu Pokemon" + p.getName() + "con un daño de" + damage);
     }
-    
+
     @Override
     public void Defense(Pokemon p) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
@@ -20,6 +20,9 @@ public class Charizard_F extends Pokemon implements IPokemon_Actions{
 
     @Override
     public void SpecialAttack(Pokemon p) {
+        int damage = getSpecialAttack() - p.getSpecialDefense(); //restar el atque a la defense del pokemon
+        int rest = p.getHP() - damage;
+        System.out.println("Tu oponente" + getName() + "ha atacado a tu Pokemon con un ataque Especial" + p.getName() + "con un daño de" + damage);
     }
 
     @Override
