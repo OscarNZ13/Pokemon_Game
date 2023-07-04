a5c1c746ca3634c96c4bf6fef4875777b12bb7a3
rodrv <valeriarodriguez09@hotmail.com>
2023-07-03 18:55:51 -0600

    Prueba Valeria_Rama

--- src/main/java/com/mycompany/pokemongame_project/PokemonGame_Project.java
+++ src/main/java/com/mycompany/pokemongame_project/PokemonGame_Project.java
@@ -10,8 +10,14 @@ public class PokemonGame_Project {
 
         Snorlax_N P1 = new Snorlax_N(AllPokemons.Snorlax, 160, 70, 60, 70, 50, PokemonType.Fire, PokemonType.Normal, PokemonType.Fire);
         Chandelure_F P2 = new Chandelure_F(AllPokemons.Chandelure, 150, 60, 50, 75, 60, PokemonType.Fire, PokemonType.Normal, PokemonType.Fire);
+        Rattata_N P3 = new Rattata_N(AllPokemons.Rattata, 160, 70, 60, 70, 50, PokemonType.Fire, PokemonType.Normal, PokemonType.Fire);
+       
         System.out.println(P1);
-        System.out.println(P2);
+        System.out.println(P2);        
+        System.out.println(P3);
+
         //Prueba de branch
+        //Prueba de branch_Valeria
+
     }
 }
